30c3f9ab232b5b3df0d9ffd3242fdd29615d4974
Your Name <you@example.com>
2022-09-15 13:20:01 +0200

    get keresek

--- index.js
+++ index.js
@@ -6,8 +6,30 @@ const app = express()
 app.use(express.json())
 const db=mysql.createConnection(config)
 
-app.use('/',(request, response)=>{
+app.get('/',(request, response)=>{
     db.query('SELECT author FROM `books` GROUP BY author order by author',(err, result)=>{
+        if(err)
+            console.log(err)
+        else
+            response.send(result)
+    })
+})
+
+app.get('/kateg/:category/:year',(request, response)=>{
+    const {category,year} = request.params
+
+    db.query('SELECT author,title FROM `books` WHERE category=? and year>=?',[category,year],(err, result)=>{
+        if(err)
+            console.log(err)
+        else
+            response.send(result)
+    })
+})
+
+app.get('/azon/:id',(request, response)=>{
+    const { id } = request.params
+
+    db.query('SELECT * FROM `books` WHERE id=? order by title',[id],(err, result)=>{
         if(err)
             console.log(err)
         else

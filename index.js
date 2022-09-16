e2e24c7eb87053104275ea53f9706d6d4a7abfa6
Your Name <you@example.com>
2022-09-16 11:39:18 +0200

    push,get stb

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/',(request, response)=>{
     })
 })
 
-app.get('/kateg/:category/:year',(request, response)=>{
+app.get('/categ/:category/:year',(request, response)=>{
     const {category,year} = request.params
 
     db.query('SELECT author,title FROM `books` WHERE category=? and year>=?',[category,year],(err, result)=>{
@@ -30,6 +30,65 @@ app.get('/azon/:id',(request, response)=>{
     const { id } = request.params
 
     db.query('SELECT * FROM `books` WHERE id=? order by title',[id],(err, result)=>{
+        if(err)
+            console.log(err)
+        else
+            response.send(result)
+    })
+})
+
+app.put('/:id/:year',(request, response)=>{
+    const {id,year}=request.params
+    db.query('UPDATE BOOKS set year=? where id=?',[year,id],(err,result)=>{
+        if(err)
+            console.log(err)
+        if(result.affectedRows==1)
+            response.send({message:"Sikeres adatmódosítás!"})
+        else
+        response.send({message:"Nem sikerült az adatmódosítás."})
+    })
+})
+
+app.put('/',(request, response)=>{
+    const {id,category,year}=request.body
+    db.query('UPDATE BOOKS set category=? , year=? where id=?',[category,year,id],(err,result)=>{
+        if(err)
+            console.log(err)
+        if(result.affectedRows==1)
+            response.send({message:"Sikeres adatmódosítás!"})
+        else
+        response.send({message:"Nem sikerült az adatmódosítás."})
+    })
+})
+
+app.post('/',(request, response)=>{
+    const {category,year,title,author}=request.body
+    db.query('insert into books values (null,?,?,?,?)',[author,title,year,category],(err,result)=>{
+        if(err)
+            console.log(err)
+        if(result)
+            response.send({message:`Sikeres adatbeírás! id:${result.insertedId}`})
+        else
+        response.send({message:"Nem sikerült az insert."})
+    })
+})
+
+app.delete('/:id',(request, response)=>{
+    const {category,year,title,author}=request.body
+    db.query('delete from books where id=?',[id],(err,result)=>{
+        if(err)
+            console.log(err)
+        if(result.affectedRows==1)
+            response.send({message:`Sikeres törlés!`})
+        else
+            response.send({message:"Nem sikerült az adattörlés."})
+    })
+})
+
+app.get('/categ/:category',(request, response)=>{
+    const {category} = request.params
+
+    db.query('SELECT author,title FROM `books` WHERE category=?',[category],(err, result)=>{
         if(err)
             console.log(err)
         else
